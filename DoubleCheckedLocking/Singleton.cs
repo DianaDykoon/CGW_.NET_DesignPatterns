3cd77661e04d10db0802c6a2a23f044a7f85e7de
Diana Dykun <diana.dykoon@gmail.com>
2024-04-11 08:50:54 +0300

    Реалізація шаблону 'DoubleCheckedLocking'

---
+++ DoubleCheckedLocking/Singleton.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace DoubleCheckedLocking
+{
+    public sealed class Singleton
+    {
+        private Singleton()
+        {
+            
+        }
+
+        private static volatile Singleton _instance;
+
+        private static readonly Object _lock = new Object();
+
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Singleton();
+                    }
+                }
+            }
+            return _instance;
+        }
+    }
+}
